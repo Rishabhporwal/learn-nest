d3f670c31a9b5f08d68cf864a327a3786c254692
Rishabh Porwal <rishabhporwal95@gmail.com>
2023-01-08 07:38:28 +0530

    basic crud API created for movies with fake database

---
+++ src/movies/movies.controller.ts
@@ -0,0 +1,50 @@
+import {
+  Controller,
+  Get,
+  Param,
+  Post,
+  Delete,
+  Body,
+  Patch,
+  Query,
+} from '@nestjs/common';
+import { Movie } from './entities/movie.entity';
+import { MoviesService } from './movies.service';
+
+@Controller('movies')
+export class MoviesController {
+  constructor(private readonly movieService: MoviesService) {}
+
+  @Get()
+  getAll(): Movie[] {
+    return this.movieService.getAll();
+  }
+
+  @Get('search')
+  search(@Query('year') year: string) {
+    return `We are searching for a movie made after ${year}`;
+  }
+
+  @Get('/:id')
+  getOne(@Param('id') movieId: string) {
+    return this.movieService.getOne(movieId);
+  }
+
+  @Post()
+  create(@Body() movieData) {
+    return this.movieService.create(movieData);
+  }
+
+  @Delete('/:id')
+  remove(@Param('id') movieId: string) {
+    return this.movieService.deleteOne(movieId);
+  }
+
+  @Patch('/:id')
+  path(@Param('id') movieId: string, @Body() updateData) {
+    return {
+      updatedMovie: movieId,
+      ...updateData,
+    };
+  }
+}
