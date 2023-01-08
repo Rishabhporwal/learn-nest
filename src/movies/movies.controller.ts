4b79faa6592d1599aa00e2ec47c0f14221fbd8c7
Rishabh Porwal <rishabhporwal95@gmail.com>
2023-01-08 17:54:36 +0530

    rest api with nest completed

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -8,6 +8,8 @@ import {
   Patch,
   Query,
 } from '@nestjs/common';
+import { CreateMovieDto } from './dto/create-movie.dto';
+import { UpdateMovieDto } from './dto/update-movie-dto';
 import { Movie } from './entities/movie.entity';
 import { MoviesService } from './movies.service';
 
@@ -21,30 +23,27 @@ export class MoviesController {
   }
 
   @Get('search')
-  search(@Query('year') year: string) {
+  search(@Query('year') year: number) {
     return `We are searching for a movie made after ${year}`;
   }
 
   @Get('/:id')
-  getOne(@Param('id') movieId: string) {
+  getOne(@Param('id') movieId: number) {
     return this.movieService.getOne(movieId);
   }
 
   @Post()
-  create(@Body() movieData) {
+  create(@Body() movieData: CreateMovieDto) {
     return this.movieService.create(movieData);
   }
 
   @Delete('/:id')
-  remove(@Param('id') movieId: string) {
+  remove(@Param('id') movieId: number) {
     return this.movieService.deleteOne(movieId);
   }
 
   @Patch('/:id')
-  path(@Param('id') movieId: string, @Body() updateData) {
-    return {
-      updatedMovie: movieId,
-      ...updateData,
-    };
+  path(@Param('id') movieId: number, @Body() updateData: UpdateMovieDto) {
+    this.movieService.update(movieId, updateData);
   }
 }
